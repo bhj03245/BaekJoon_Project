965d63cc25a43822df3eff22d48a3be525c274ff
bhj03245 <bhj03245@naver.com>
2020-07-04 22:37:32 +0900

    Croatian Alphabet

---
+++ Step8/BaekJoon_2941.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    char word[101];
+    scanf("%s", word);
+
+    int i;
+    int cnt = 0;
+    for (i = 0;; i++) {
+        char lett = word[i];
+        if (word[i] == NULL)
+            break;
+
+        if (lett == 'n' || lett == 'l') {
+            if (word[i + 1] == 'j') continue;
+        }
+        else if (lett == 'c' || lett == 's' || lett == 'z') {
+            if (word[i + 1] == '=' || word[i + 1] == '-') continue;
+        }
+        else if (lett == 'd') {
+            if (word[i + 1] == '-') continue;
+            if (word[i + 1] == 'z' && word[i + 2] == '=') continue;
+        }
+
+        cnt++;
+    }
+    printf("%d", cnt);
+}
